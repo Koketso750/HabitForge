84f5eef9f4c2e172dbebcfdfe7ec009032a4a4eb
Koketso Matome <koketsomatome750@gmail.com>
2025-02-25 20:08:09 +0200

    1st Commit

---
+++ src/main/java/swp/habitforge/habitforge/HabitforgeApplication.java
@@ -0,0 +1,13 @@
+package swp.habitforge.habitforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitforgeApplication.class, args);
+	}
+
+}
